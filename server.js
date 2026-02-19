c4daa083fc0ab1b7ce1b6b9f4acf2432ae566d6a
nathan <vinteo.ac@gmail.com>
2026-02-19 09:10:36 -0500

    message topic seul

--- server.js
+++ server.js
@@ -36,6 +36,11 @@ cloudinary.config({
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
+console.log("CLOUDINARY CONFIG CHECK:", {
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key_present: !!process.env.CLOUDINARY_API_KEY,
+  api_secret_present: !!process.env.CLOUDINARY_API_SECRET
+});
 
 const upload = multer({ storage: multer.memoryStorage() });
 
